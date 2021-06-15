b7b23a095f4276c18bdf381e97baccc92a1d7689
cy <1052144407@qq.com>
2021-06-15 21:55:26 +0800

    home主页-举报功能完成

--- src/api/article.js
+++ src/api/article.js
@@ -18,3 +18,29 @@ export const getArticleList = (id, timestamp) => {
     }
   })
 }
+
+/**
+ * 对文章不喜欢
+ * @param {Number} id 文章id
+ * @returns promise
+ */
+export const dislikeArticle = id => {
+  return axios.post('/v1_0/article/dislikes', { target: id })
+}
+
+/**
+ * 举报文章
+ * @param {Number} id 文章id
+ * @param {Number} type 举报类型
+ * @returns promise
+ */
+export const reportArticle = (id, type) => {
+  return axios({
+    method: 'post',
+    url: '/v1_0/article/reports',
+    data: {
+      target: id,
+      type
+    }
+  })
+}
--- src/views/Layout/Home/ArticleList.vue
+++ src/views/Layout/Home/ArticleList.vue
@@ -39,17 +39,41 @@
                 <span>{{ item.aut_name }}</span>
                 <span>{{ item.comm_count }}评论</span>
                 <span>{{ item.pubdate | fromNow }}</span>
+                <!-- 更多操作按钮 -->
+                <span
+                  class="close"
+                  style="float: right"
+                  v-if="$store.state.user.token.token"
+                  @click="showMoreAction(item.art_id)"
+                >
+                  <van-icon name="cross"></van-icon>
+                </span>
               </div>
             </div>
           </template>
         </van-cell>
+        <!-- 更多操作弹出层 -->
+        <van-popup v-model="isShowMoreAction" :style="{ width: '60%' }">
+          <!-- 一级操作 -->
+          <van-cell-group v-if="isTop">
+            <van-cell @click="dislike">不感兴趣</van-cell>
+            <van-cell is-link @click="isTop = false">反馈垃圾内容</van-cell>
+            <van-cell @click="blacklist">拉黑作者</van-cell>
+          </van-cell-group>
+          <!-- 二级操作 -->
+          <van-cell-group v-else>
+            <van-cell icon="arrow-left" @click="isTop = true">返回</van-cell>
+            <van-cell v-for="item in reports" :key="item.value" @click="report(item.value)">{{ item.label }}</van-cell>
+          </van-cell-group>
+        </van-popup>
       </van-list>
     </van-pull-refresh>
   </div>
 </template>
 
 <script>
-import { getArticleList } from '@/api/article'
+import { getArticleList, dislikeArticle, reportArticle } from '@/api/article'
+import { reports } from '@/api/constants'
 export default {
   props: {
     channelId: Number
@@ -60,7 +84,11 @@ export default {
       loading: false,
       finished: false,
       refreshing: false,
-      timestamp: Date.now()
+      timestamp: Date.now(),
+      isShowMoreAction: false,
+      isTop: true,
+      artId: '',
+      reports
     }
   },
   methods: {
@@ -68,13 +96,13 @@ export default {
       if (this.refreshing) {
         this.articleList = []
         this.timestamp = Date.now()
-        this.refreshing = false
       }
       const res = await getArticleList(this.channelId, this.timestamp)
       // console.log(res)
       this.timestamp = res.data.pre_timestamp
       this.articleList = [...this.articleList, ...res.data.results]
       this.loading = false
+      this.refreshing = false
       if (res.data.results.length === 0 || this.articleList.length > 100) {
         this.finished = true
       }
@@ -88,6 +116,28 @@ export default {
       this.finished = false
       this.loading = true
       this.onLoad()
+    },
+    showMoreAction (artId) {
+      this.artId = artId
+      // 每次点击显示都显示一级操作
+      this.isTop = true
+      this.isShowMoreAction = true
+    },
+    async dislike () {
+      // console.log(this.artId)
+      await dislikeArticle(this.artId)
+      this.isShowMoreAction = false
+      this.$toast.success('操作成功')
+      this.articleList = this.articleList.filter(item => item.art_id !== this.artId)
+    },
+    async report (value) {
+      await reportArticle(this.artId, value)
+      this.$toast.success('举报成功')
+      this.isShowMoreAction = false
+    },
+    blacklist () {
+      this.$toast.success('拉黑成功')
+      this.isShowMoreAction = false
     }
   }
 }
